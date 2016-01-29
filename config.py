f3f5d29f244eab6ebaff5535d96be18b1c23015a
InJung Chung <mu29gl@gmail.com>
2016-01-29 18:34:09 +0900

    설정 파일

---
+++ config.py
@@ -0,0 +1,19 @@
+#-*- coding: utf-8 -*-
+
+FB_ID = 'mu29@naver.com'
+FB_PASS = ''
+
+USER_INFO = {
+    'user_id' : 'wjdwnd95',
+    'user_password' : ''
+}
+
+KEYWORD = [
+    "도적",
+    "럭퍼",
+    "팬텀",
+    "메이플",
+]
+
+LOWER_THAN = 10001
+HIGHER_THAN = 100000
